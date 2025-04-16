eb41136bc8a4f53f8e4477c219f3f393c622154e
Ron-Sungcang <sungcangjoseph@gmail.com>
2025-04-15 19:29:32 -0600

    seperated user prompts into new file backend/app/models/prompts.py

---
+++ backend/app/models/prompts.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+#Pydantic model for user prompt
+class UserPrompt(BaseModel):
+    name: str = None
+    message: str
+    subject: str
+    date: datetime = Field(default_factory=datetime.timezone.utc)
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
 from typing import List
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
+    "http://localhost:5174",
 ]
 
 app.add_middleware(
@@ -18,21 +19,25 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
-#Pydantic model for request body
-class Item(BaseModel):
-    name: str
-    description: str
-    price: float
-    tax: float = None
+#Pydantic model for user prompt
+class userPrompt(BaseModel):
+    name: str = None
+    message: str
+    subject: str
+    date: datetime = Field(default_factory=datetime.timezone.utc)
 
 #Dummy endpoints
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return {"name": item.name, "price": item.price}
+@app.get("/app/message")
+async def message(prompt: userPrompt):
+    return {"message": prompt.message}
+
+@app.post("/app/messages")
+async def create_item(prompt: userPrompt):
+    return {"name": prompt.name, "messahe": prompt.message}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
